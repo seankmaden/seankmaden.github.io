a930fa9cb774467d7c1aa329467cb17bfc86b07b
Sean Maden <seankmaden@gmail.com>
2025-06-10 10:48:52 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,37 +1,5 @@
 // script.js
 
-function openPage(pageName) {
-    closeNav();
-    const tabContents = document.getElementsByClassName('tab-content');
-    for (const content of tabContents) {
-        content.style.display = 'none';
-    }
-
-    document.getElementById(pageName).style.display = 'block';
-}
-
-// Set the default tab (e.g., 'home')
-openPage('home');
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
-}
-
-// scroll box script, from year in text 2024
-document.querySelector('.scrollbox').addEventListener('scroll', function() { console.log('Scrolling...'); });
-function openPage(pageName) {
-    closeNav();
-    const tabContents = document.getElementsByClassName('tab-content');
-    for (const content of tabContents) {
-        content.style.display = 'none';
-    }
-    document.getElementById(pageName).style.display = 'block';
-}
-
 document.getElementById("menuButton1").addEventListener("click", function() {
     let menu = document.getElementById("menu1");
     menu.classList.toggle("expanded");
@@ -51,3 +19,35 @@ document.getElementById("menuButton4").addEventListener("click", function() {
     let menu = document.getElementById("menu4");
     menu.classList.toggle("expanded");
 });
+
+function openPage(pageName) {
+    closeNav();
+    const tabContents = document.getElementsByClassName('tab-content');
+    for (const content of tabContents) {
+        content.style.display = 'none';
+    }
+
+    document.getElementById(pageName).style.display = 'block';
+}
+
+// Set the default tab (e.g., 'home')
+openPage('home');
+
+function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0px";
+}
+
+// scroll box script, from year in text 2024
+document.querySelector('.scrollbox').addEventListener('scroll', function() { console.log('Scrolling...'); });
+function openPage(pageName) {
+    closeNav();
+    const tabContents = document.getElementsByClassName('tab-content');
+    for (const content of tabContents) {
+        content.style.display = 'none';
+    }
+    document.getElementById(pageName).style.display = 'block';
+}
