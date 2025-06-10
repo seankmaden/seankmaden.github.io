6049b60d2010009ed5c764dbfb6540b2da832ed2
Sean Maden <seankmaden@gmail.com>
2025-06-10 10:03:00 -0700

    moves scripts, styles, rm dropdown_* files

--- scripts.js
+++ scripts.js
@@ -31,3 +31,23 @@ function openPage(pageName) {
     }
     document.getElementById(pageName).style.display = 'block';
 }
+
+document.getElementById("menuButton1").addEventListener("click", function() {
+    let menu = document.getElementById("menu1");
+    menu.classList.toggle("expanded");
+});
+
+document.getElementById("menuButton2").addEventListener("click", function() {
+    let menu = document.getElementById("menu2");
+    menu.classList.toggle("expanded");
+});
+
+document.getElementById("menuButton3").addEventListener("click", function() {
+    let menu = document.getElementById("menu3");
+    menu.classList.toggle("expanded");
+});
+
+document.getElementById("menuButton4").addEventListener("click", function() {
+    let menu = document.getElementById("menu4");
+    menu.classList.toggle("expanded");
+});
